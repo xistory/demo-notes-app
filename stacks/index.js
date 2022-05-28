6f130716b7f992c6f665ff376b2791bd21e6d37d
xistory <ximinseo@hotmail.com>
2022-05-28 14:15:12 +0900

    renew

---
+++ stacks/index.js
@@ -0,0 +1,14 @@
+import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
+
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "backend",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+}
